c7a4271c55e08acbb613cf3639ff40e207accfbf
elfesyaesen <web.elfesyaesen@gmail.com>
2022-12-14 17:22:12 +0300

    kurucu class ile veritabanı bağlantısı...

---
+++ crud.class.php
@@ -0,0 +1,31 @@
+<?php 
+    class VeritabaniSinifi
+    {
+        /* VERİTABANI BAĞLANTISI İÇİN GEREKLİ PARAMETRELER TANIMLANDI.. */
+        protected $pdo = null;
+        protected $host = 'localhost';
+        protected $user = 'root';
+        protected $pass = '';
+        protected $db   = 'test';
+        protected $charset = 'utf8';
+        protected $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        /* VERİTABANI BAĞLANTI KURULUMU */
+        public function __construct()
+        {
+            $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+            try {
+             $this->pdo = new PDO($dsn, $this->user, $this->pass, $this->options);
+            } catch (\PDOException $e) {
+             throw new \PDOException($e->getMessage(), (int)$e->getCode());
+            }
+        }
+        
+
+    }
+
+
+?>
