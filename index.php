90aa16859ab0d63fcef89f1d642160365dddb9c2
EFE ESEN (Elfesya) <49937779+elfesyaesen@users.noreply.github.com>
2023-08-08 00:12:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ require_once 'crud.class.php';
 /* SINIFIMIZI KULLANABİLMEK İÇİN YENİ BİR NESNE TÜRETTİK... */
 $app = new PhpYardimlasmaGrubu;
 /* KOŞULSUZ SELECT 
-$sql = "SELECT * FROM ch_test_table";
-$request = $app->pdoMultipleSelect($sql);
+$sql = "SELECT * FROM products";
+$request = $app->pdoQueryMultiple($sql);
 var_dump($request);
 */
 
 /* KOŞULLU SELECT 
 $sql = "SELECT * FROM ch_test_table WHERE id =:id";
-$params = ['id' => '2'];
-$request = $app->pdoSelect($sql, $params);
+$args = $app->getSecurity(['id' => '1']);
+$request = $app->pdoPrepareSingle($sql, $params);
 var_dump($request);
 */
