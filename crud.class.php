d0cc04961e7def7d8cfd0ca330db0191a4f43340
EFE ESEN (Elfesya) <49937779+elfesyaesen@users.noreply.github.com>
2023-08-08 20:56:19 +0300

    Update crud.class.php
    metin kısaltma fonksiyonu eklendi

--- crud.class.php
+++ crud.class.php
@@ -1,7 +1,6 @@
-<?php
-class PhpYardimlasmaGrubu
-{
-    /* VERİTABANI BAĞLANTISI İÇİN GEREKLİ PARAMETRELER TANIMLANDI.. */
+<?php 
+class PhpYardimlasmaGrubu {
+    //veritabanı bağlantı bilgileri
     protected $pdo = null;
     protected $host = "localhost";
     protected $user = "root";
@@ -13,9 +12,8 @@ class PhpYardimlasmaGrubu
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     ];
-    /* VERİTABANI BAĞLANTI KURULUMU */
-    public function __construct()
-    {
+    //kurucu class ta otomatik veritabanı bağlantısı
+    public function __construct() {
         $dsn = "mysql:host=$this->host;dbname=$this->dbname;charset=$this->charset";
         try {
             $this->pdo = new PDO(
@@ -28,126 +26,59 @@ class PhpYardimlasmaGrubu
             throw new \PDOException($e->getMessage(), (int) $e->getCode());
         }
     }
-    //db veri ekleme
-    public function pdoPrepareInsert($sql, $args = [])
-    {
+    //veritabanı veri ekleme, son eklenen id değerini döndürür
+    public function pdoPrepareInsert($sql, $args = []) {
         $prepare = $this->pdo->prepare($sql);
         $prepare->execute($args);
         $result = $prepare->fetch(PDO::FETCH_ASSOC);
         $lastInsertID = $this->pdo->lastInsertID();
-        if ($lastInsertID) {
-            return true;
-        } else {
-            return false;
-        }
+        if ($lastInsertID) { return true; } else { return false; }
     }
-    //db veri güncelleme
-    public function pdoPrepareUpdate($sql, $args = [])
-    {
+    //veritabanı veri güncelleme, etkilenen satır sayısını döndürür
+    public function pdoPrepareUpdate($sql, $args = []) {
         $prepare = $this->pdo->prepare($sql);
         $prepare->execute($args);
         $result = $prepare->fetch(PDO::FETCH_ASSOC);
         $rowCount = $prepare->rowCount();
-        if ($rowCount) {
-            return true;
-        } else {
-            return false;
-        }
+        if ($rowCount) { return true; } else { return false; }
     }
-    //db veri silme
-    public function pdoPrepareDelete($sql, $args = [])
-    {
+    //veritabanı veri silme, etkilenen satır sayısını döndürür
+    public function pdoPrepareDelete($sql, $args = []) {
         $prepare = $this->pdo->prepare($sql);
         $prepare->execute($args);
         $result = $prepare->fetch(PDO::FETCH_ASSOC);
         $rowCount = $prepare->rowCount();
-        if ($rowCount) {
-            return true;
-        } else {
-            return false;
-        }
+        if ($rowCount) { return true; } else { return false; }
     }
-    //db tekli veri çekme
-    public function pdoQuerySingle($sql)
-    {
+    //veritabanı tekli veri çekme, sadece bir kayıt döndürür
+    public function pdoQuerySingle($sql) {
         $query = $this->pdo->query($sql);
         $result = $query->fetch(PDO::FETCH_ASSOC);
-        if ($result) {
-            return $result;
-        } else {
-            return false;
-        }
+        if ($result) { return $result; } else { return false; }
     }
-    //db çoklu veri çekme
-    public function pdoQueryMultiple($sql)
-    {
+    //veritabanı çoklu veri çekme, tablodaki bütün veriyi döndürür
+    public function pdoQueryMultiple($sql) {
         $query = $this->pdo->query($sql);
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
-        if ($result) {
-            return $result;
-        } else {
-            return false;
-        }
+        if ($result) { return $result; } else { return false; }
     }
-    //tarih fonksiyonu
-    public function getDate()
-    {
-        return date("Y.m.d H:i:s");
+    //verdiğiniz kritere göre tekli veri çekme
+    public function pdoPrepareSingle($sql, $args = []){
+        $prepare = $this->pdo->prepare($sql);
+        $prepare->execute($args);
+        $result = $prepare->fetch(PDO::FETCH_ASSOC);
+        if ($result) { return $result; } else { return false; }
     }
-    //metin kısaltma
-    public function shortenText($text, $maxLength, $suffix = '...') {
-        if (strlen($text) > $maxLength) {
-            $shortenedText = substr($text, 0, $maxLength - strlen($suffix)) . $suffix;
-            return $shortenedText;
-        }
-    return $text;
-    }
-    //input boş mu kontrol
-    public function pdoEmpty($variable)
-    {
-        if (is_array($variable)) {
-            foreach ($variable as $value) {
-                if (is_null($value)) {
-                    return false;
-                } elseif (empty($value)) {
-                    return false;
-                }
-            }
-        } elseif (is_null($variable)) {
-            return false;
-        } elseif (empty($variable)) {
-            return false;
-        }
-        return true;
-    }
-
-    //koşullu çoklu veri çekme
-    public function pdoPrepareMultiple($sql, $args = [])
-    {
+    //verdiğiniz kritere göre çoklu veri çekme
+    public function pdoPrepareMultiple($sql, $args = []){
         $prepare = $this->pdo->prepare($sql);
         $prepare->execute($args);
         $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
-        if ($result) {
-            return $result;
-        } else {
-            return false;
-        }
+        if ($result) { return $result; } else { return false; }
     }
-    //koşullu tekli veri çekme
-    public function pdoPrepareSingle($sql, $args = [])
-    {
-        $prepare = $this->pdo->prepare($sql);
-        $prepare->execute($args);
-        $result = $prepare->fetch(PDO::FETCH_ASSOC);
-        if ($result) {
-            return $result;
-        } else {
-            return false;
-        }
-    }
-    //resim yükleme
-    public function upload($image_input_name, $upload_path)
-    {
+
+    //yükleyeceğiniz klasör yolu ve image input ismini verdiğinizde resim yükler ve resmin adını döndürür
+    public function upload($image_input_name, $upload_path) {
         $input = $image_input_name;
         $klasor = $upload_path;
         $target_dir = $klasor;
@@ -161,13 +92,9 @@ class PhpYardimlasmaGrubu
             $_FILES[$input]["tmp_name"],
             $yeniyol
         );
-        if ($image_upload) {
-            return $basename;
-        } else {
-            return false;
-        }
+        if ($image_upload) { return $basename; } else { return false; }
     }
-    //güvenlik fonksiyonu
+    //güvenlik fonksiyonu, sql injectiona karşı sitenizi korur post get ve sql saldırılarında güvendesiniz 
     public function getSecurity($data)
     {
         if (is_array($data)) {
@@ -216,5 +143,34 @@ class PhpYardimlasmaGrubu
             return $respon;
         }
     }
-
+    //bugünün tarihini döndürür
+    public function getDate() {
+        return date("Y.m.d H:i:s");
+    }
+    //belli karekter sonra üç nokta koyarak metni kıslatır
+    public function shortenText($text, $maxLength, $suffix = '...') {
+        if (strlen($text) > $maxLength) {
+            $shortenedText = substr($text, 0, $maxLength - strlen($suffix)) . $suffix;
+            return $shortenedText;
+        }
+    return $text;
+    }
+    //post ve get değerlerinin boş olup olmadığını kontrol eder , dizi veya tekli veriyi kontrol eder
+    public function pdoEmpty($variable)
+    {
+        if (is_array($variable)) {
+            foreach ($variable as $value) {
+                if (is_null($value)) {
+                    return false;
+                } elseif (empty($value)) {
+                    return false;
+                }
+            }
+        } elseif (is_null($variable)) {
+            return false;
+        } elseif (empty($variable)) {
+            return false;
+        }
+        return true;
+    }
 }
